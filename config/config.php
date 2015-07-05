f34077bccc203eb8bf01aa263e622da0783416b5
Shane Perera <shane@shaneperera.com>
2015-07-05 12:14:37 +1000

    Fix the config issues

--- config/config.php
+++ config/config.php
@@ -4,6 +4,8 @@ return array(
     'list_id' => '',
 
     //installation url :)
+    // add the full base URL without the ending slash
+    // eg http://www.example-domain.com/sendy
     'installation_url' => '',
 
     //and the API key... :)
